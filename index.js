f2b3a15fa2150dae5398299d34e71e7dbeba9860
Godlyysek <lukynkob@gmail.com>
2022-02-27 11:38:07 +0100

    grid complete, most functions complete

--- index.js
+++ index.js
@@ -1,315 +1,292 @@
 //
 
+class Node {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.isWall;
+        this.parent;
+        this.object;
+        this.isStart;
+        this.isEnd;
+
+        this.gCost = Infinity;
+        this.hCost;
+        this.fCost;
+    }
+}
+
 let size = 15;
 
 const stepLinear = 10;
 const stepDiagonal = 14;
 
+let nodeArray = new Array();
 
-class Node {
+let table = document.getElementById('game');
 
-    constructor(x, y) {
 
-        this.x = x;
-        this.y = y;
-        this.parent;
-        this.object;
-        this.fCost;
-        this.gCost;
-        this.hCost;
-        this.isWall = false;
+function createArea() {
+
+    for (let row = 0; row < size; row++) {
+
+        let tempRowNodeArr = new Array();
+        let tempRowObjectArr = document.createElement('tr');
+
+        for (let col = 0; col < size; col++) {
+            let tempNode = new Node(row, col);
+            let tempObject = document.createElement('td');
+            tempObject.setAttribute('id', `${row},${col}`);
+            tempObject.setAttribute('class', 'square');
+            tempObject.setAttribute('onclick', 'clickNode(this.id)');
+            tempRowObjectArr.appendChild(tempObject);
+            tempNode.object = tempObject;
+
+            if (row == 0 || row == (size - 1) || col == 0 || col == (size - 1)) {
+                tempObject.classList.add('wall');
+                tempNode.isWall = true;
+            }
+            tempRowNodeArr.push(tempNode);
+        }
+        nodeArray.push(tempRowNodeArr);
+
+        table.appendChild(tempRowObjectArr);
     }
-
 }
+createArea();
 
+let wasStartSelected = false;
+let wasEndSelected = false;
+let wasGameStarted = false;
 
 let startNode;
 let endNode;
 
-let objectArray = new Array();
+function clickNode(id) {
 
-let nodes = new Array();
+    let clicked = document.getElementById(id);
 
-let table = document.getElementById('game');
-
-function createTable() {
-
-    for (let i = 0; i < size; i++) {
-        let _row = [];
-        let row = document.createElement('tr');
-        let _nodes = new Array();
-        for (let a = 0; a < size; a++) {
-            let n = new Node(i, a);
-            _nodes.push(n);
-
-            let obj = document.createElement('td');
-            obj.setAttribute('class', 'square temp');
-            obj.setAttribute('id', `${i},${a}`);
-            // obj.innerHTML = `${i},${a}`;
-            obj.setAttribute('onclick', 'elementClicked(this.id)');
-            n.object = obj;
-            row.appendChild(obj);
-            _row.push(obj);
-        }
-        objectArray.push(_row);
-        nodes.push(_nodes);
-        table.appendChild(row);
-    }
-}
-
-function assignCost() {
-
-    for (let row = 0; row < nodes.length; row++) {
-        for (let col = 0; col < nodes[row].length; col++) {
-            nodes[row][col].gCost = calculateDistance(nodes[row][col], startNode);
-            nodes[row][col].hCost = calculateDistance(nodes[row][col], endNode);
-        }
-
-    }
-
-}
-
-
-let clickedStart = false;
-let clickedEnd = false;
-let isRunning = false;
-
-
-
-function elementClicked(id) {
-
-    if (!clickedStart) {
-
-        let _t = document.getElementById(id);
-
-        for (let row = 0; row < nodes.length; row++) {
-            for (let col = 0; col < nodes[row].length; col++) {
-                nodes[row][col].object.classList.remove('temp');
-                nodes[row][col].object.classList.add('after');
-            }
-        }
-
-        for (let row = 0; row < nodes.length; row++) {
-            for (let col = 0; col < nodes[row].length; col++) {
-
-                if (nodes[row][col].object == _t) {
-                    startNode = nodes[row][col];
-                    startNode.object.style.backgroundColor = 'rgb(67, 255, 34)';
-                    clickedStart = true;
-                    return
-                }
-            }
-        }
-
-
-
-    } else if (!clickedEnd) {
-
-        let _t = document.getElementById(id);
-
-        if (!(_t == startNode.object)) {
-
-            for (let row = 0; row < nodes.length; row++) {
-                for (let col = 0; col < nodes[row].length; col++) {
-                    nodes[row][col].object.classList.remove('after');
-                    nodes[row][col].object.classList.add('a');
+    if (!wasStartSelected) {
+        for (let i = 0; i < nodeArray.length; i++) {
+            for (let j = 0; j < nodeArray[i].length; j++) {
+                if (clicked == nodeArray[i][j].object) {
+                    nodeArray[i][j].isStart = true;
+                    startNode = nodeArray[i][j];
+                    nodeArray[i][j].object.classList.add('start-square');
                 }
             }
 
+        }
+        wasStartSelected = true;
+        return;
+    } else if (!wasEndSelected) {
+        for (let i = 0; i < nodeArray.length; i++) {
+            for (let j = 0; j < nodeArray[i].length; j++) {
+                if (clicked == nodeArray[i][j].object) {
+                    nodeArray[i][j].isEnd = true;
+                    endNode = nodeArray[i][j];
+                    nodeArray[i][j].object.classList.add('end-square');
+                }
+            }
 
-            for (let row = 0; row < nodes.length; row++) {
-                for (let col = 0; col < nodes[row].length; col++) {
-
-                    if (nodes[row][col].object == _t) {
-                        endNode = nodes[row][col];
-                        endNode.object.style.backgroundColor = 'rgb(255, 0, 0)';
-                        clickedEnd = true;
-                        return
+        }
+        wasEndSelected = true;
+        return;
+    } else if (!wasGameStarted) {
+        for (let i = 0; i < nodeArray.length; i++) {
+            for (let j = 0; j < nodeArray[i].length; j++) {
+                if (clicked == nodeArray[i][j].object) {
+                    if (clicked != endNode.object && clicked != startNode.object) {
+                        nodeArray[i][j].isWall = true;
+                        nodeArray[i][j].object.classList.add('wall');
                     }
                 }
             }
+
         }
-    } else if (!isRunning) {
-        let _t = document.getElementById(id);
-
-        for (let row = 0; row < nodes.length; row++) {
-            for (let col = 0; col < nodes[row].length; col++) {
-
-                if (nodes[row][col].object == _t) {
-                    nodes[row][col].object.style.backgroundColor = 'grey';
-                    nodes[row][col].isWall = true;
-                }
-            }
-        }
-
     }
+
 
 }
 
-createTable();
+function calculateDistance(currentNode, targetNode) {
+    let xDiff = Math.abs(currentNode.x - targetNode.x);
+    let yDiff = Math.abs(currentNode.y - targetNode.y);
+
+    let rem = Math.abs(xDiff - yDiff);
+    currentNode.object.innerHTML = `${stepDiagonal * Math.min(xDiff, yDiff) + stepLinear * rem}`
+    return stepDiagonal * Math.min(xDiff, yDiff) + stepLinear * rem;
+}
 
 let tempNode = new Node(null, null);
 tempNode.fCost = Infinity;
 
+function calculateSmallestCost(list) {
+    console.log('LIST HERE');
+    console.log(list);
+    let smallest = tempNode;
+    list.forEach(node => {
+        if (node.fCost < tempNode.fCost) {
+            smallest = node;
+        }
+        console.log(node);
+    });
+    console.log(smallest.fCost);
+    return smallest;
+}
+
 
 function startGame() {
+    wasGameStarted = true;
 
     let open = new Array();
     let closed = new Array();
-
     open.push(startNode);
 
     startNode.gCost = 0;
     startNode.hCost = calculateDistance(startNode, endNode);
-    startNode.fCost = startNode.hCost + startNode.gCost;
+    startNode.fCost = startNode.gCost + startNode.hCost;
 
-
-    function getSmallestCost() {
-        tempNode.fCost = Infinity;
-        for (let i = 0; i < open.length; i++) {
-            if (open[i].fCost < tempNode.fCost) {
-                tempNode = open[i];
-            }
-        }
-        return tempNode;
-    }
-
-    function calculateCosts(n) {
-
-        let gc = calculateDistance(n, startNode);
-        let hc = calculateDistance(n, endNode);
-        n.gCost = gc;
-        n.hCost = hc;
-        n.fCost = gc + hc;
-
-    }
-
-
-    while (open.length > 0) {
-        let currentNode = getSmallestCost();
+    let temp = 3;
+    while (temp > 0) {
+        currentNode = calculateSmallestCost(open);
         currentNode.object.innerHTML = `${currentNode.fCost}`;
-        currentNode.object.style.backgroundColor = 'yellow';
-
-        let a = sleep(5);
-
-        if (currentNode == endNode) {
-            console.log('found');
-            break;
-        }
-
         open.splice(open.indexOf(currentNode));
         closed.push(currentNode);
 
+        if (currentNode == endNode) {
+            console.log('found end node.');
+            break;
+        }
+
+
         let neighbours = getNeighbours(currentNode);
+        console.log(neighbours);
 
-        neighbours.forEach(element => {
-            // setTimeout(3000);
-            if (closed.includes(element) || element.isWall) {} else {
+        for (let neig = 0; neig < neighbours.length; neig++) {
+            neighbours[neig].gCost = calculateDistance(neighbours[neig], startNode);
+            neighbours[neig].hCost = calculateDistance(neighbours[neig], endNode);
+            neighbours[neig].fCost = neighbours[neig].gCost + neighbours[neig].hCost;
 
-                calculateCosts(element);
-                element.object.innerHTML = `${element.fCost}`;
-                element.parent = currentNode;
-                let newCost = currentNode.gCost + calculateDistance(currentNode, element);
-                console.log('NEW NODE');
-                console.log(currentNode);
-                console.log(element);
-                console.log(`current:${currentNode.gCost}, new:${newCost}`);
-
-                if (newCost < element.gCost) {
-                    console.log('a');
-                    element.parent = currentNode;
-                    element.gCost = newCost;
-                    element.hCost = calculateDistance(element, endNode);
-                    element.fCost = element.hCost + element.gCost;
-                }
-                if (!(open.includes(element))) {
-                    open.push(element);
-                }
+            let newCost = currentNode.gCost + calculateDistance(neighbours[neig], currentNode);
+            if (newCost < calculateDistance(neighbours[neig], endNode)) {
+                neighbours[neig].gCost = newCost;
+                neighbours[neig].parent = currentNode;
             }
-        });
-    }
-
-    findPath();
-    startNode.object.style.backgroundColor = 'rgb(67, 255, 34)';
-    endNode.object.style.backgroundColor = 'red';
-
-    function findPath() {
-        let node = endNode;
-        while (node != startNode) {
-            node.object.style.backgroundColor = 'aqua';
-            node = node.parent;
+            if (!(open.includes(neighbours[neig])) || closed.includes(neighbours[neig])) {
+                open.push(neighbours[neig]);
+            }
         }
+
+        temp--;
     }
 }
 
+function getNeighbours(node) {
+    let array = new Array();
 
-function calculateDistance(currNode, targetNode) {
+    if (node.x - 1 > 0) {
+        if (!(nodeArray[node.x - 1][node.y].isWall)) {
+            array.push(nodeArray[node.x - 1][node.y]);
+        }
+        if (node.y - 1 > 0) {
+            if (!(nodeArray[node.x - 1][node.y - 1].isWall)) {
+                array.push(nodeArray[node.x - 1][node.y - 1]);
+            }
+        }
+        if (node.y + 1 < size) {
+            if (!(nodeArray[node.x - 1][node.y + 1].isWall)) {
+                array.push(nodeArray[node.x - 1][node.y + 1]);
+            }
+        }
+    }
+    if (node.x + 1 < size) {
+        if (!(nodeArray[node.x + 1][node.y].isWall)) {
+            array.push(nodeArray[node.x + 1][node.y]);
+        }
+        if (node.y + 1 < size) {
+            if (!(nodeArray[node.x + 1][node.y + 1].isWall)) {
+                array.push(nodeArray[node.x + 1][node.y + 1]);
+            }
 
-    let xDiff = Math.abs(currNode.x - targetNode.x);
-    let yDiff = Math.abs(currNode.y - targetNode.y);
+        }
+        if (node.y - 1 > 0) {
+            if (!(nodeArray[node.x + 1][node.y - 1].isWall)) {
+                array.push(nodeArray[node.x + 1][node.y - 1]);
+            }
+        }
+    }
+    if (node.y - 1 > 0) {
+        if (!(nodeArray[node.x][node.y - 1].isWall)) {
+            array.push(nodeArray[node.x][node.y - 1]);
+        }
+    }
+    if (node.y + 1 < size) {
+        if (!(nodeArray[node.x][node.y + 1].isWall)) {
+            array.push(nodeArray[node.x][node.y + 1]);
+        }
+    }
 
-    let rem = Math.abs(xDiff - yDiff);
-    // currNode.object.innerHTML = `${stepDiagonal * Math.min(xDiff, yDiff) + stepLinear * rem}`
-    return stepDiagonal * Math.min(xDiff, yDiff) + stepLinear * rem;
+    return array;
 }
 
 
-function getNeighbours(currN) {
 
-    let arr = new Array();
 
-    if (currN.x - 1 >= 0) {
 
-        arr.push(nodes[currN.x - 1][currN.y]);
-        // console.log(currN);
-        // console.log(nodes[currN.x - 1][currN.y]);
-        if (currN.y - 1 >= 0) {
-            arr.push(nodes[currN.x - 1][currN.y - 1]);
-        }
-        if (currN.y + 1 <= size) {
-            arr.push(nodes[currN.x - 1][currN.y + 1]);
-        }
-    }
 
-    if (currN.x + 1 <= size) {
-        arr.push(nodes[currN.x + 1][currN.y]);
-        if (currN.y - 1 >= 0) {
-            arr.push(nodes[currN.x + 1][currN.y - 1]);
-        }
-        if (currN.y + 1 <= size) {
-            arr.push(nodes[currN.x + 1][currN.y + 1]);
-        }
-    }
-    if (currN.y - 1 >= 0) {
-        arr.push(nodes[currN.x][currN.y - 1]);
-    }
-    if (currN.y + 1 <= size) {
-        arr.push(nodes[currN.x][currN.y + 1]);
-    }
-    return arr;
-}
+let nodes = new Array();
 
-// function getNode(n) {
 
-//     for (let row = 0; row < objectArray.length; row++) {
-//         for (let col = 0; col < objectArray[row].length; col++) {
-//             if (row == n.x && col == n.y) {
-//                 return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function selectNode(oldClassName, newClassName) {
+
+//     if (!wasStartSelected) {
+//         for (let row = 0; row < nodeArray.length; row++) {
+
+//             for (let col = 0; col < nodeArray[row].length; col++) {
+//                 let n = nodeArray[row][col];
+//                 if (n.isWall) {
+//                     continue;
+//                 } else {
+//                     if (oldClassName != null) {
+//                         n.object.classList.remove(oldClassName);
+//                     }
+//                     n.object.classList.add(newClassName);
+//                 }
 //             }
-
-
 //         }
-
 //     }
-
-
 // }
-
-function sleep(duration) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.log('sleep');
-            resolve()
-        }, duration * 1000)
-    })
-}
